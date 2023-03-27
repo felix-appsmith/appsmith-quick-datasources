a08bfdc52b9afd4dd95d6f2523961488e5c004d5
Félix Suárez <felix@appsmith.com>
2023-03-27 14:00:26 -0600

    chore: update script sql

--- sqlserver/seed.sql
+++ sqlserver/seed.sql
@@ -1,34 +1,42 @@
-USE master
-GO
-
-IF DB_ID('develop') IS NOT NULL
-BEGIN
-    ALTER DATABASE develop SET SINGLE_USER WITH ROLLBACK IMMEDIATE
-    DROP DATABASE develop
-END
-
-CREATE DATABASE develop;
-GO
-
-USE develop;
-GO
+USE master;
 
 CREATE TABLE users (
-    id INT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    age INT,
-    email VARCHAR(100),
-    created_at DATETIME DEFAULT GETDATE()
+id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+username VARCHAR(50) NOT NULL,
+email VARCHAR(100) NOT NULL,
+age INT,
+bio TEXT,
+avatar VARBINARY(MAX),
+is_active BIT,
+created_at DATETIME2 DEFAULT CURRENT_TIMESTAMP,
+data NVARCHAR(MAX),
+salary DECIMAL(10, 2)
 );
 
 DECLARE @counter INT = 1;
+
 WHILE @counter <= 1000
 BEGIN
-    INSERT INTO users (id, name, age, email) VALUES (
-        @counter,
-        'User ' + CAST(@counter AS VARCHAR),
-        ABS(CHECKSUM(NEWID())) % 80 + 18, 
-        'user' + CAST(@counter AS VARCHAR) + '@example.com'
+    INSERT INTO master.dbo.users (username, email, age, bio, avatar, is_active, created_at, data, salary)
+    VALUES (
+        CONCAT('user', @counter),
+        CONCAT('user', @counter, '@example.com'),
+        ROUND(RAND() * 50 + 20, 0),
+        CONCAT('I am a ',
+            CASE WHEN RAND() < 0.5 THEN 'software developer' ELSE 'teacher' END,
+            ' from ',
+            CASE WHEN RAND() < 0.5 THEN 'New York' ELSE 'Los Angeles' END
+        ),
+        NULL,
+        CAST(RAND() * 2 AS BIT),
+        GETDATE(),
+        '{ "key1": "value1", "key2": "value2" }',
+        ROUND(RAND() * 50000 + 5000, 2)
     );
     SET @counter += 1;
-END
+END;
+
+
+
+
+
